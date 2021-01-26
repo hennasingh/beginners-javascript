8056ffadd1f7182a785e90799d52ecd46a75b619
hennasingh <hennasingh04@gmail.com>
2021-01-26 05:25:16 +0000

    Javascript Playground

---
+++ playground/functions.js
@@ -0,0 +1,12 @@
+// functions
+/* eslint-disable */
+
+Math.max();
+Math.max(10, 12);
+
+parseInt('20.34324343')
+
+Date.now() // number of milisecs after Jan 1 something
+
+
+
---
+++ playground/types.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+// 3 ways to create Strings
+
+const name = 'wes';
+const middle = 'topher';
+const last = `bos`;
+
+const sentence = "she's so \"cool\"";
+const sentence2 = `she's so "cool"`;
+
+//to use backticks for text in multiple lines or apostopres and commas
+
+
+//Numbers
+Math.floor(21.5)
+Math.ceil(21.999)
+Math.round(20.5)
+Math.random()
+
+//Everything in JS is Objects
+const person = {
+    first: 'Henna',
+    last: 'Singh',
+    age: 33
+}
+
+//Acronyms for Types = SNOBUS
+/* There is a difference between Undefined and Null that value has to be explicitly assigned null while when its not
+there it is termed as Undefined
+*/
+
+//BOOLEANS
+/* Double Equals (==) compares only Values
+while Triple Equals (===) compares both values and types
+*/
+
